a754fd1376300273caa70421ae9e22fd731b79d5
Alexander Scholz <alexander.scholz@uni-oldenburg.de>
2021-02-25 18:33:39 +0100

    Embed tsx

---
+++ jukebox.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="jukebox" tilewidth="32" tileheight="32" tilecount="48" columns="4">
+ <image source="jukebox.png" width="144" height="384"/>
+</tileset>
