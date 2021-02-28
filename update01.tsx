fc0b150864d253f423a8c5913fcbbf3ac86ba7e0
Alexander Scholz <alexander.scholz@uni-oldenburg.de>
2021-02-28 18:52:51 +0100

    Ergänzungen

---
+++ update01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="update01" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="tavern inn by CBL/sprites/update01.png" width="256" height="256"/>
+</tileset>
