5f4fd25c2069ff74a427a0034c144cc0f4f82111
Alexander Scholz <alexander.scholz@uni-oldenburg.de>
2021-03-01 11:48:23 +0100

    Collider

---
+++ AmongUs_Transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="AmongUs_Transparent" tilewidth="256" tileheight="224" tilecount="1" columns="1">
+ <image source="img/AmongUs_Transparent.png" width="258" height="253"/>
+</tileset>
---
+++ among-us-apk.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="among-us-apk" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="img/among-us-apk.jpg" width="304" height="307"/>
+</tileset>
